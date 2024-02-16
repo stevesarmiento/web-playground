603baccd3cb9e4f43dc1137b72deedd159a847b4
steven <sarmiento.steven@gmail.com>
2024-02-15 19:14:21 -0500

    buttonssss

--- src/app/components/button.tsx
+++ src/app/components/button.tsx
@@ -1,23 +1,19 @@
 'use client'
 
-import { FunctionComponent } from 'react';
+import { FunctionComponent, useState } from 'react';
 import React from 'react';
 import styled from '@emotion/styled';
 
 const StyledButton = styled.button`
 position: relative;
-background-color: #333A56; 
+background: linear-gradient(to bottom, #21273F, #333A56);
 height: 150px;
 width: 150px;
 color: white;
 border-radius: 20px;
-border: 1px solid rgba(26, 33, 106, 1);
-font-size: 18px; 
-font-weight: bold; 
 transition: background-color 0.2s ease, transform 0.2s ease-in-out;
-box-shadow: 0 5px 10px rgba(51, 17, 255, 0.22), 0 1px 3px rgba(26, 33, 106, 0.8), 0 1px 0px rgba(28, 33, 102, 1);
+box-shadow: 0 20px 10px rgba(0, 0, 0, 0.5), 0 1px 4px rgba(0, 0, 0, 0.8), 0 1px 0px rgba(0, 0, 0, 1);
 overflow: hidden;
-text-shadow: 0 1px 0px rgba(0, 0, 0, 0.4);
 
 ::after {
   content: '';
@@ -26,41 +22,43 @@ text-shadow: 0 1px 0px rgba(0, 0, 0, 0.4);
   left: 0px;
   right: 0px;
   bottom: 0px;
-  border: 1px solid rgba(255, 255, 255, 0.2);
-  // box-shadow: inset 0 -1px 0px rgb(0, 0, 0, 0.3);
-  border-radius: 17px;
+  border: 0px solid rgba(19, 21, 36, 1);
+  box-shadow: inset 0 1px 1px rgb(255, 255, 255, 0.1), inset 0 -5px 0px rgb(0, 0, 0, 0.5);
+  border-radius: 20px;
   pointer-events: none; 
-  background: linear-gradient(to bottom, transparent, transparent, rgba(255, 255, 255, 0)); 
 }
 
 ::before {
   content: '';
   position: absolute;
+  height: 1px;
+  width: 100px;
   top: 0;
-  left: 0; 
+  left: 20%; 
   right: 0; 
-  bottom: 20px; 
-  border-radius: 20px; 
-  scale: 103%;
-  background: rgba(255, 255, 255, 0.08);
+  bottom: 0px; 
+  background: radial-gradient(circle, rgba(255, 255, 255, 0.3) 0%, rgba(255, 255, 255, 0) 100%); // little notch highlight at top
   z-index: 0;
 }
 
-:hover::before {
-  bottom: 22px;
-  border-radius: 10px; 
+:active::before {
+  background: radial-gradient(circle, rgba(255, 255, 255, 0) 0%, rgba(255, 255, 255, 0) 100%);
   transition: all 0.2s ease, transform 0.2s ease-in-out;
-
 }
 
 :active {
+  background: linear-gradient(to bottom, #21273F, #333A56);
   transform: scale(0.99);
-  box-shadow: 0 5px 10px rgba(51, 17, 255, 0.22), 0 1px 3px rgba(26, 33, 106, 0.8), 0 0 0 rgba(28, 33, 102, 1);
-
+  box-shadow: inset 0 10px 20px rgba(0, 0, 0, 0.777), inset 0 1px 1px rgb(0, 0, 0, 1) ;
 }
 
 :hover {
-  background-color: #4A53B8; 
+  //background: linear-gradient(to bottom, #21273F, #333A56);
+}
+
+svg {
+  width: 4em;
+  height: 4em; 
 }
   }
 `
@@ -78,13 +76,15 @@ const ButtonSkeu: FunctionComponent<ButtonProps> = ({
   return (
     
     <>
-      <div className="">
-        <StyledButton
-          className={`${className} flex justify-center text-center items-center`}
-          {...props}
-         >
-          <p className="">{children}</p>
-        </StyledButton>
+      <div className="border-[10px] border-black/60 rounded-[39px]">
+        <div className="bg-black/80 border-[8px] rounded-[28px] border-black/40 ring-[8px] ring-[#191e2c] focus:ring-[#FF9900]">
+          <StyledButton
+            className={`${className} flex justify-center text-center items-center`}
+            {...props}
+          >
+            {children}
+          </StyledButton>
+        </div>
       </div>
     </>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,16 @@
-import Image from "next/image";
 import ButtonSkeu from "./components/button";
-import { Play } from "lucide-react";
+import { Pause, Play, Square } from "lucide-react";
 
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-white">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <ButtonSkeu><Play /></ButtonSkeu>
+    <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-[#151724]">
+      <div className="z-10 max-w-5xl w-full items-center justify-between lg:flex">
+        <div className="mx-auto bg-[#202739] border-[1px] border-white/10 p-6 rounded-[50px] inline-flex space-x-6 shadow-xl shadow-black/30">
+          <ButtonSkeu><Play className="fill-white/80 text-white/0" /></ButtonSkeu>
+          <ButtonSkeu><Pause className="fill-white/80 text-white/0" /></ButtonSkeu>
+          <ButtonSkeu><Square className="fill-white/80 text-white/0" /></ButtonSkeu>
+        </div>
       </div>
     </main>
   );
